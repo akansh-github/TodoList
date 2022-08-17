64136505382d7f8a2fdcc35a48cafe4382e3d132
akansh-github <akanshbende@gmail.com>
2022-08-17 11:48:34 +0530

    TodoList using React

---
+++ src/App.js
@@ -0,0 +1,19 @@
+//rafce
+import React from 'react';
+// import Resturant from "./components/basics/Resturant";
+// import UseState from './components/basics/Hooks/useState'
+// import UseEffect from "./components/basics/Hooks/useEffect";
+// import { UseReducer } from "./components/basics/Hooks/useReducer";
+import  Todo from "./components/basics/todoreact/todo"
+const App = () => {
+  return (
+    // <Resturant /> //it is a component
+    //  
+    // <UseEffect />,
+
+    // <UseReducer />
+    <Todo />
+  )
+}
+
+export default App
